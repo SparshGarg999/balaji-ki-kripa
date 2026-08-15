657084562b4b0e2a405094173c86707e5b6ad112
Sparsh :) <76697238+SparshGarg999@users.noreply.github.com>
2026-08-15 09:49:57 +0530

    feat: add sw.js for offline PWA support

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'balaji-kripa-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
